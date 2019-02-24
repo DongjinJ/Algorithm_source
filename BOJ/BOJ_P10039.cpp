3c77f6cd8fc44aaa7a2d5217d47a9b17e226c8ae
Dongjin <exon91@naver.com>
2019-02-24 23:29:47 +0900

    2019.02.24

---
+++ BOJ/BOJ_P10039.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int answer = 0;
+
+    for(int i = 0;i < 5;i++){
+        int temp;
+        scanf("%d", &temp);
+        if(temp < 40)
+            answer += 40;           // 40점 이하는 보충학습을 통해 40점을 받게됨
+        else
+            answer += temp;         // 그 이외에는 본인 점수 입력
+    }
+    answer /= 5;            // 인원 수 만큼 나누기(평균)
+
+    printf("%d", answer);
+
+    return 0;
+
+}
